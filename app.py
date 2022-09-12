dc9d2f876c05746316b42f7adbb64b33fea6a41d
Milan Mitrovic <milanmitrovic1991@gmail.com>
2022-09-13 00:34:42 +0200

    new navbar 2

--- app.py
+++ app.py
@@ -3,65 +3,85 @@ import dash
 import pandas as pd
 import dash_mantine_components as dmc
 from data_etl import df_raw
-from utils import create_metric_chooser, create_top_n_filter, create_city_filter, create_category_filter, \
-    create_vendor_filter, create_bottly_volume_filter, create_simple_grid
-import dash_mantine_components as dmc
+from utils import (
+    create_metric_chooser,
+    create_top_n_filter,
+    create_city_filter,
+    create_category_filter,
+    create_vendor_filter,
+    create_bottly_volume_filter,
+    create_simple_grid,
+)
 
 app = Dash(__name__, use_pages=True)
 
-app.layout = dmc.Container([
+sidebar = dmc.Navbar(
+    fixed=True,
+    style={"marginLeft": 25},
+    width={"base": 300},
+    height=300,
+    children=[
+        dmc.ScrollArea(
+            offsetScrollbars=True,
+            type="scroll",
+            children=[
+                dmc.Group(
+                    direction="column",
+                    children=[
+                        dmc.Button("Filters", id="modal-demo-button"),
+                        dmc.Modal(
+                            size="lg",
+                            title="Dataset Filters",
+                            id="modal",
+                            children=create_simple_grid(
+                                component_list=[
+                                    create_metric_chooser(),
+                                    create_top_n_filter(),
+                                    create_city_filter(),
+                                    create_category_filter(),
+                                    create_vendor_filter(),
+                                    create_bottly_volume_filter(),
+                                ]
+                            ),
+                        ),
+                    ],
+                ),
+                dmc.Divider(style={"marginBottom": 20, "marginTop": 20}),
+                dmc.Group(
+                    direction="column",
+                    children=[
+                        dcc.Link(
+                            dmc.Text(page["name"], size="sm", color="gray"),
+                            href=page["path"],
+                            id=page["name"],
+                            style={"textDecoration": "none"},
+                        )
+                        for page in dash.page_registry.values()
+                    ],
+                ),
+            ],
+        )
+    ],
+)
 
-    dmc.Navbar(
-        p="md",
-        width={"base": 150},
-        height=1000,
-        children=[
 
-            dmc.Button("Filters", id="modal-demo-button"),
-            dmc.Modal(
-                size='lg',
-                title="Dataset Filters",
-                id="modal",
-                children=create_simple_grid(
-                    component_list=[
-
-                        create_metric_chooser(),
-
-                        create_top_n_filter(),
-
-                        create_city_filter(),
-
-                        create_category_filter(),
-
-                        create_vendor_filter(),
-
-                        create_bottly_volume_filter()
-
-                    ]),
-            ),
-
-            html.Br(),
-
-
-            dcc.Link("Home", href="/"),
-            html.Br(),
-            dcc.Link("bar_charts", href="/bar-chart-page"),
-            html.Br(),
-            dcc.Link("line_charts", href="/line-charts"),
-            html.Br(),
-            dcc.Link("download_page", href="/download-page"),
-
-        ],
-        fixed=True
-
-    ),
-
-    html.H1('Dash Autumn Challange', style={'textAlign': 'center'}),
-
-    html.Br(),
-
-    dash.page_container
-])
+app.layout = dmc.Container(
+    [
+        dmc.Header(
+            height=40,
+            children=[html.H1("Dash Autumn Challange", style={"textAlign": "center"})],
+            style={"backgroundColor": "#FFFFFF"},
+        ),
+        sidebar,
+        dmc.Container(
+            dash.page_container,
+            size="lg",
+            pt=20,
+            style={"marginLeft": 350},
+        ),
+    ],
+    fluid=True,
+)
 
 
 @callback(
@@ -74,5 +94,5 @@ def modal_demo(nc1, opened):
     return not opened
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
